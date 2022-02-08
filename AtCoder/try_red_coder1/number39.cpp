d64b4de36f400f46ed6c1658b8a88fc84a20a8db
keigotoyo <keigo.work199987s@gmail.com>
2022-02-08 20:09:22 +0900

    number39

---
+++ AtCoder/try_red_coder1/number39.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <cmath>
+#include<math.h>
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INFL = 1LL << 60;
+const ll inf_ll = 1e15;
+int inf_int = 2147483647;
+using mpair = pair<int,int>;
+
+int main(){
+  
+}
