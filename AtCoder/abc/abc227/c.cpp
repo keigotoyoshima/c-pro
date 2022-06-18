b57281bc89a75a8392ae4cbd2d9f0c6cc6ff5d3c
keigotoyo <keigo.work199987s@gmail.com>
2022-06-18 11:26:03 +0900

    227_c

--- AtCoder/abc/abc227/c.cpp
+++ AtCoder/abc/abc227/c.cpp
@@ -1,21 +1,49 @@
-#include <iostream>
+#include<iostream>
 #include <vector>
 #include <cstdlib>
-#include <algorithm>
 #include <map>
+#include <set>
 #include <algorithm>
+#include <numeric>
+#include <queue>
+#include <deque>
 #include <cmath>
+#include <math.h>
+#include <string>
+#include <assert.h>
 using namespace std;
 using ll = long long;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+int inf_int = 2147483647;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
 
-const long long INFL = 1LL << 60;
-const int inf = 1e6;
 
-const ll MAX = pow(2,62);
+
 int main(){
-  
+  // const double MAX = pow 2,63);
+  ll n; cin >> n;
+  ll ans = 0;
+  for (ll a = 1; a*a*a <= n; a++)
+  {
+    for (ll b = a; a*b*b <= n; b++)
+    {
+      ll max_c = n/(a*b);
+      ans += max_c - b + 1;
+    }
+  }
+  cout << ans << endl;
+
+    
   return 0;
   
 }
