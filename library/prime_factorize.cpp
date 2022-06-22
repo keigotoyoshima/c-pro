df940fe608dd573254a3ee3296b055952afb4720
keigotoyo <keigo.work199987s@gmail.com>
2022-06-22 11:15:24 +0900

    素因数分解　やり方

--- library/prime_factorize.cpp
+++ library/prime_factorize.cpp
@@ -1,14 +1,26 @@
-#include <stdio.h>
-#include <stdint.h>
-#include<vector>
-#include <string>
-#include <set>
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <map>
+#include <set>
 #include <algorithm>
-#include <map>  
+#include <numeric>
+#include <queue>
+#include <deque>
+#include <cmath>
 #include <math.h>
-using namespace std;	
+#include <string>
+#include <assert.h>
+using namespace std;
 using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+int inf_int = 2147483647;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
 
 // 素因数分解は O(N‾‾√) で実現できる。
@@ -23,4 +35,19 @@ vector<pair<ll, ll> > prime_factorize(ll n) {
     }
     if (n != 1) res.push_back(make_pair(n, 1));
     return res;
+}
+
+int main(){
+    ll n = 12;
+    // 2^2 * 3
+    vector<pair<ll, ll>> res = prime_factorize(n);
+    for(auto v: res){
+        ll fi = v.first;
+        ll se = v.second;
+        cout << fi << " " << se << endl;
+    }
+    // 2 2
+    // 3 1
+
+    return 0;
 }
