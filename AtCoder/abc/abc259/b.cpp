9a09ae71a513fe2cac18f36e8adf341f1761a1d4
keigotoyo <keigo.work199987s@gmail.com>
2022-07-14 09:49:22 +0900

    空

---
+++ AtCoder/abc/abc259/b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <math.h>
+#include <string>
+#include <assert.h>
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+int inf_int = 2147483647;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+
+
+
+int main(){
+  return 0;
+}
