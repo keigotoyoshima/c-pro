9a42ca6d8765cb27616e146c7bba13a83cf7df27
keigotoyo <keigo.work199987s@gmail.com>
2022-07-14 12:39:21 +0900

    解き直し

--- AtCoder/Educational_DP_Contest/b.cpp
+++ AtCoder/Educational_DP_Contest/b.cpp
@@ -1,51 +1,71 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <algorithm>
 #include <map>
 #include <set>
 #include <algorithm>
 #include <numeric>
 #include <queue>
+#include <deque>
 #include <cmath>
-#include<math.h>
+#include <math.h>
+#include <string>
+#include <assert.h>
 using namespace std;
 using ll = long long;
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-const long long INFL = 1LL << 60;
-const ll inf_ll = 1e15;
+#define fi first
+#define se second
+template <class T>
+inline bool chmax(T &a, T b)
+{
+  if (a < b)
+  {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+inline bool chmin(T &a, T b)
+{
+  if (a > b)
+  {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+const long long INF = 1LL << 60;
 int inf_int = 2147483647;
-using mpair = pair<int,int>;
 const int dx[] = {-1, 0, 1, 0};
 const int dy[] = {0, 1, 0, -1};
 const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
-
-int main(){
-  int n,k; cin >> n >> k;
-  vector<int>h(n);
+int main()
+{
+  int n,k;
+  cin >> n  >> k;
+  vector<int> h(n);
   for (int i = 0; i < n; i++)
   {
     cin >> h[i];
   }
-  ll dp[100010];
-  
-  for (int i = 0; i < 100010; i++)
-  {
-    dp[i] = inf_ll;
-  }
+  vector<int> dp(n, inf_int);
   dp[0] = 0;
   for (int i = 0; i < n; i++)
   {
     for (int j = 1; j <= k; j++)
     {
-      if(i+j >= n) continue;
-      chmin(dp[i+j], dp[i] + abs(h[i] - h[i+j]));
+      if(i+j < n)chmin(dp[i+j], dp[i]+ abs(h[i+j] - h[i]));
     }
 
   }
-  cout << dp[n-1] << endl;
+  // for(auto v: dp){
+  //   cout << v << " ";
+  // }
+  // cout << endl;
+  cout << dp[n - 1] << endl;
+
   return 0;
 }
