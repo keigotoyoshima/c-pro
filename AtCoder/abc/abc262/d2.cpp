77e5bcce731d9e1f6da60f6332ff316857eb8b49
keigotoyo <keigo.work199987s@gmail.com>
2022-08-07 15:26:26 +0900

    dpの初期化をmemsetを用いて,　関数呼ぶ度に行う． MOD取る時は足した後に全体からMODをとる． addmodスニペット追加

--- AtCoder/abc/abc262/d2.cpp
+++ AtCoder/abc/abc262/d2.cpp
@@ -18,8 +18,26 @@ using namespace std;
 using ll = long long;
 #define fi first
 #define se second
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template <class T>
+inline bool chmax(T &a, T b)
+{
+  if (a < b)
+  {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T>
+inline bool chmin(T &a, T b)
+{
+  if (a > b)
+  {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
 const long long INF = 1LL << 60;
 int inf_int = 2147483647;
 const int MOD = 998244353;
@@ -29,45 +47,56 @@ const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
 // n=1,2,3...に対して
 // dp[i][j][k] = i-1番目まででj個選んで、選んだ数の和をnで割ったあまりがkになる方法の数
-const ll N = 110;
-ll a[N], n;
-ll dp[N][N][N];
+ll n;
+vector<ll> a;
+ll dp[110][110][110];
 
-void add(ll &x, ll y)
-{
+void addmod(ll &x, ll y){
   x += y;
   x %= MOD;
 }
 
-ll solve(ll len){
+ll solve(ll cnt)
+{
+  //先頭から2バイト進めた位置に「１」を3バイト書き込む
+  // char buf[] = "ABCDEFGHIJK";
+  // memset(buf + 2, '1', 3);
+  // AB111FGHIJK
+
   memset(dp, 0, sizeof(dp));
-  for (int i = 0; i <= n; i++)
+  dp[0][0][0] = 1;
+  for (int i = 1; i <= n; i++)
   {
-    dp[i][0][0] = 1;
-    if(i == 0) continue;
-    for (int left = 1; left <= len; left++)
+    for (int j = 0; j <= i; j++)
     {
-      for (int rem = 0; rem < len; rem++)
+
+      for (int k = 0; k < cnt; k++)
       {
-        add(dp[i][left][rem], dp[i - 1][left][rem]);
-        add(dp[i][left][rem], dp[i - 1][left - 1][(((rem - a[i]) % len) + len) % len]);
+        ll tem =  (((k-a[i-1])%cnt)+cnt)%cnt;
+        if (j > 0)
+        {
+          addmod(dp[i][j][k], dp[i - 1][j - 1][tem]);
+        }
+        addmod(dp[i][j][k], dp[i - 1][j][k]);
       }
     }
   }
 
-  return dp[n][len][0];
+  return dp[n][cnt][0];
 }
 
 int main()
 {
   cin >> n;
-  for(int i = 1; i <= n; i++){
-    cin  >> a[i];
-  }
+  a.resize(n);
+  for (auto &v : a)
+    cin >> v;
   ll sum = 0;
-  for(int i = 1; i <= n; i++){
-    add(sum,solve(i));
+  for (int i = 1; i <= n; i++)
+  {
+    addmod(sum,solve(i));
   }
   cout << sum << endl;
+
   return 0;
 }
