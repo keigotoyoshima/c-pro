990cb25bc351938ee2562574d754077101dfa031
keigotoyo <keigo.work199987s@gmail.com>
2022-08-07 14:59:15 +0900

    完成形

--- AtCoder/abc/abc262/d2.cpp
+++ AtCoder/abc/abc262/d2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <cstring>
 #include <map>
 #include <set>
 #include <algorithm>
@@ -27,89 +28,46 @@ const int dy[] = {0, 1, 0, -1};
 const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
 // n=1,2,3...に対して
-// dp[i][j][k] = i-1番目まででj個選んで、選んだ数の和をnで割ったあまりがkになる方法の数 
-ll n;
-vector<ll>a;
-int dp[101][100][100];
+// dp[i][j][k] = i-1番目まででj個選んで、選んだ数の和をnで割ったあまりがkになる方法の数
+const ll N = 110;
+ll a[N], n;
+ll dp[N][N][N];
 
-void init(){
-  for(int i = 0; i < 101; i++){
-    for(int j = 0; j < 100; j++){
-       for(int k = 0; k < 100; k++){
-          dp[i][j][k] = 0;
-     }
-   }
-  }
+void add(ll &x, ll y)
+{
+  x += y;
+  x %= MOD;
 }
 
-int solve(ll cnt){
-  init();
-  dp[0][0][0] = 1;
-  for (int i = 1; i <= n; i++)
+ll solve(ll len){
+  memset(dp, 0, sizeof(dp));
+  for (int i = 0; i <= n; i++)
   {
-    for (int j = 0; j <= i; j++)
+    dp[i][0][0] = 1;
+    if(i == 0) continue;
+    for (int left = 1; left <= len; left++)
     {
-
-      for (int k = 0; k < cnt; k++)
+      for (int rem = 0; rem < len; rem++)
       {
-        // dp[i][j][k] = 0;
-        // cout << "row " <<  cnt << " " << j << " " << k << endl;
-        int tem = (cnt + k - a[i - 1] % cnt) % cnt;
-        // cout << k << " " << tem << endl;
-        if (j > 0){ 
-          dp[i][j][k] += dp[i - 1][j - 1][tem] % MOD;
-          // cout << "yes" << endl; 
-        }
-
-        // cout << dp[i][j][k] << endl;
-        dp[i][j][k] += dp[i - 1][j][k] % MOD;
-        // cout << dp[i-1][j][k] << endl;
-        // cout << dp[i][j][k] << endl;
+        add(dp[i][left][rem], dp[i - 1][left][rem]);
+        add(dp[i][left][rem], dp[i - 1][left - 1][(((rem - a[i]) % len) + len) % len]);
       }
     }
   }
 
-  // cout << "cnt: " << cnt << endl;
-  // cout << dp[n][cnt][0] << endl;
-
-  // cout << endl;
-  // for(int i = 0; i < 5; i++){
-  //   for(int j = 0; j < 5; j++){
-  //      for(int k = 0; k < 5; k++){
-  //         cout << dp[i][j][k] << ' ';
-  //    }
-  //    cout << endl;
-  //  }
-  //  cout << endl;
-  // }
-  // cout << endl;
-  return dp[n][cnt][0];
+  return dp[n][len][0];
 }
 
 int main()
 {
   cin >> n;
-  a.resize(n);
-  for(auto &v: a) cin >> v;
+  for(int i = 1; i <= n; i++){
+    cin  >> a[i];
+  }
   ll sum = 0;
   for(int i = 1; i <= n; i++){
-    sum += solve(i)%MOD;
+    add(sum,solve(i));
   }
   cout << sum << endl;
-
-  // cout << endl;
-  // for(int i = 0; i < 5; i++){
-  //   for(int j = 0; j < 5; j++){
-  //      for(int k = 0; k < 5; k++){
-  //         cout << dp[i][j][k] << ' ';
-  //    }
-  //    cout << endl;
-  //  }
-  //  cout << endl;
-  // }
-  // cout << endl;
-
-
-
   return 0;
 }
