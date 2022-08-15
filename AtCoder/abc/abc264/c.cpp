257fe31d9dca2419300e385c1fb2258a87347af1
keigotoyo <keigo.work199987s@gmail.com>
2022-08-15 11:53:28 +0900

    264

--- AtCoder/abc/abc264/c.cpp
+++ AtCoder/abc/abc264/c.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <cstring>
 #include <map>
 #include <set>
 #include <algorithm>
@@ -12,6 +13,7 @@
 #include <math.h>
 #include <string>
 #include <assert.h>
+#include <functional>
 using namespace std;
 using ll = long long;
 #define fi first
@@ -27,30 +29,10 @@ const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
 
-void addmod(int &x, int y){
-  x += y;
-  x %= MOD;
-}
 
-// bitDP
-int dp[10000000];
+
+
 int main(){
-  int n; cin >> n;
-  vector<vector<int>>G(n, vector<int>(n,0));
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < n; j++){
-      cin >> G[i][j];
-    }
-  }
-  dp[0] = 1;
-  for(int S = 1; S < (1<<n); S++){
-    int cur = __builtin_popcount(S);
-    for(int i = 0; i < n; i++){
-      if((S>>i)&1 && G[cur-1][i] == 1){
-        addmod(dp[S], dp[S^(1<<(i))]);
-      }
-    } 
-  }
-  cout << dp[(1<<n)-1] << endl;
+  
   return 0;
 }
