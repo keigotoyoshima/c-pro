1a1207d74c5ae929694938c46f63b2f8bc486e96
keigotoyo <keigo.work199987s@gmail.com>
2022-08-15 14:44:02 +0900

    cout matrix2スニペット追加

--- AtCoder/abc/abc264/c.cpp
+++ AtCoder/abc/abc264/c.cpp
@@ -30,9 +30,56 @@ const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
 
 
+bool isSame(vector<vector<int>>a, vector<vector<int>>b){
+  if(a.size() != b.size() || a[0].size() != b[0].size()) return false;
+  for(int i = 0; i < a.size(); i++){
+    for(int j = 0; j < a[0].size(); j++){
+      if(a[i][j] != b[i][j]) return false;
+    }
+  }
+  return true;
+}
 
 
 int main(){
+  int h1,w1; cin >> h1 >> w1;
+  vector<vector<int>>a(h1,vector<int>(w1));
+  for(int i = 0; i < h1; i++){
+    for(int j = 0;j < w1; j++){
+      cin >> a[i][j];
+    }
+  }
+  int h2,w2; cin >> h2 >> w2;
+  vector<vector<int>>b(h2,vector<int>(w2));
+  for(int i = 0; i < h2; i++){
+    for(int j = 0;j < w2; j++){
+      cin >> b[i][j];
+    }
+  }
   
+  for(int H = 0; H < (1<<h1); H++){
+    for(int W = 0; W < (1<<w1); W++){
+      int height = __builtin_popcount(H);
+      int width = __builtin_popcount(W);
+
+      vector<vector<int>>tem_grid;
+
+      for(int i = 0; i < h1; i++){
+        vector<int>tem_row;
+        for(int j = 0; j < w1; j++){
+          if((H>>i)&1 && (W>>j)&1) tem_row.push_back(a[i][j]);
+        }
+        if(tem_row.size() != 0){
+          tem_grid.push_back(tem_row);
+        }
+      }
+      // coutGrid(tem_grid);
+      if(isSame(tem_grid, b)){
+        cout << "Yes" << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "No" << endl;
   return 0;
 }
