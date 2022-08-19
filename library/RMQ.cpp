eed918ed7f9de7040bee35ae616b939b3041fe46
keigotoyo <keigo.work199987s@gmail.com>
2022-08-19 16:17:16 +0900

    区間更新型の遅延セグメント木+二分探索で計算量改善

--- library/RMQ.cpp
+++ library/RMQ.cpp
@@ -34,93 +34,116 @@ https://algo-logic.info/segment-tree/#toc_id_1_1
 右の子 : dat[2*i+2]
 配列の i 番目が表すノードdat[i]の親
 親 : dat[(i-1)/2]
-
 /* RMQ：[0,n-1] について、区間ごとの最小値を管理する構造体
-  update(a,b,x): 区間[a,b) の要素を x に更新。O(log(n))
-  query(a,b): [a,b) での最小の要素を取得。O(log(n))
+    set(int i, T x), build(): i番目の要素をxにセット。まとめてセグ木を構築する。O(n)
+    update(i,x): i 番目の要素を x に更新。O(log(n))
+    query(a,b): [a,b) での最小の要素を取得。O(log(n))
+    find_rightest(a,b,x): [a,b) で x以下の要素を持つ最右位置を求める。O(log(n))
+    find_leftest(a,b,x): [a,b) で x以下の要素を持つ最左位置を求める。O(log(n))
 */
 template <typename T>
 struct RMQ
 {
-  const T INF = numeric_limits<T>::max();
+  const T e = numeric_limits<T>::max();
+  function<T(T, T)> fx = [](T x1, T x2) -> T
+  { return min(x1, x2); };
   int n;
-  vector<T> dat, lazy;
-  RMQ(int n_) : n(), dat(n_ * 4, INF), lazy(n_ * 4, INF)
+  vector<T> dat;
+  RMQ(int n_) : n(), dat(n_ * 4, e)
   {
     int x = 1;
     while (n_ > x)
+    {
       x *= 2;
+    }
     n = x;
   }
 
-  /* lazy eval */
-  void eval(int k) // 配列のk番目を更新
+  void set(int i, T x) { dat[i + n - 1] = x; }
+  void build()
   {
-    if (lazy[k] == INF)
-      return; // 更新するものが無ければ終了
-    if (k < n - 1)
-    { // 葉でなければ子に伝搬
-      lazy[k * 2 + 1] = lazy[k];
-      lazy[k * 2 + 2] = lazy[k];
-    }
-    // 自身を更新
-    dat[k] = lazy[k];
-    lazy[k] = INF;
+    for (int k = n - 2; k >= 0; k--)
+      dat[k] = fx(dat[2 * k + 1], dat[2 * k + 2]);
   }
-  
-  // 区間更新
-  void update(int a, int b, T x, int k, int l, int r)
+
+  void update(int i, T x)
   {
-    // 遅延評価
-    eval(k);
-    if (a <= l && r <= b)
-    { // 完全に内側の時
-      lazy[k] = x;
-      eval(k);
-    }
-    else if (a < r && l < b)
-    {                                             // 一部区間が被る時
-      update(a, b, x, k * 2 + 1, l, (l + r) / 2); // 左の子
-      update(a, b, x, k * 2 + 2, (l + r) / 2, r); // 右の子
-      dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+    i += n - 1;
+    dat[i] = x;
+    while (i > 0)
+    {
+      i = (i - 1) / 2; // parent
+      dat[i] = fx(dat[i * 2 + 1], dat[i * 2 + 2]);
     }
   }
 
-  void update(int a, int b, T x) { update(a, b, x, 0, 0, n); }
-
-  //  [a,b)の最小値を取得する
-  // k:現在見ているノードの位置  [l,r):dat[k]が表している区間
+  // the minimum element of [a,b)
+  T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
   T query_sub(int a, int b, int k, int l, int r)
   {
-    eval(k);
     if (r <= a || b <= l)
-    { // 完全に外側の時
-      return INF;
+    {
+      return e;
     }
     else if (a <= l && r <= b)
-    { // 完全に内側の時
+    {
       return dat[k];
     }
     else
-    { // 一部区間が被る時
+    {
       T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
       T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
-      return min(vl, vr);
+      return fx(vl, vr);
     }
   }
-  T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
 
-  /* debug */
-  inline T operator[](int a) { return query(a, a + 1); }
-  void print()
+  int find_rightest(int a, int b, T x) { return find_rightest_sub(a, b, x, 0, 0, n); }
+  int find_leftest(int a, int b, T x) { return find_leftest_sub(a, b, x, 0, 0, n); }
+  int find_rightest_sub(int a, int b, T x, int k, int l, int r)
   {
-    for (int i = 0; i < 2 * n - 1; ++i)
-    {
-      cout << (*this)[i];
-      if (i != n)
-        cout << ",";
+    if (dat[k] > x || r <= a || b <= l)
+    { // 自分の値がxより大きい or [a,b)が[l,r)の範囲外ならreturn a-1
+      return a - 1;
     }
-    cout << endl;
+    else if (k >= n - 1)
+    { // 自分が葉ならその位置をreturn
+      return (k - (n - 1));
+    }
+    else
+    {
+      int vr = find_rightest_sub(a, b, x, 2 * k + 2, (l + r) / 2, r);
+      if (vr != a - 1)
+      { // 右の部分木を見て a-1 以外ならreturn
+        return vr;
+      }
+      else
+      { // 左の部分木を見て値をreturn
+        return find_rightest_sub(a, b, x, 2 * k + 1, l, (l + r) / 2);
+      }
+    }
+  }
+  int find_leftest_sub(int a, int b, T x, int k, int l, int r)
+  {
+    if (dat[k] > x || r <= a || b <= l)
+    { // 自分の値がxより大きい or [a,b)が[l,r)の範囲外ならreturn b
+      return b;
+    }
+    else if (k >= n - 1)
+    { // 自分が葉ならその位置をreturn
+      return (k - (n - 1));
+    }
+    else
+    {
+      int vl = find_leftest_sub(a, b, x, 2 * k + 1, l, (l + r) / 2);
+      if (vl != b)
+      { // 左の部分木を見て b 以外ならreturn
+        return vl;
+      }
+      else
+      { // 右の部分木を見て値をreturn
+        return find_leftest_sub(a, b, x, 2 * k + 2, (l + r) / 2, r);
+      }
+    }
   }
 };
 
