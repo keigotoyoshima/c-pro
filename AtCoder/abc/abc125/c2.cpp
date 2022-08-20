c33e26b9640b7d337b025637a829c038f3200299
keigotoyo <keigo.work199987s@gmail.com>
2022-08-20 15:09:34 +0900

    遅延セグメント木で高速にする

---
+++ AtCoder/abc/abc125/c2.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <assert.h>
+#include <functional>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+int inf_int = 2147483647;
+const int MOD = 1000000007;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+template <typename X, typename M>
+struct SegTreeLazy {
+    using FX = function<X(X, X)>;
+    using FA = function<X(X, M)>;
+    using FM = function<M(M, M)>;
+    int n;
+    FX fx;
+    FA fa;
+    FM fm;
+    const X ex;
+    const M em;
+    vector<X> dat;
+    vector<M> lazy;
+    SegTreeLazy(int n_, FX fx_, FA fa_, FM fm_, X ex_, M em_)
+        : n(), fx(fx_), fa(fa_), fm(fm_), ex(ex_), em(em_), dat(n_ * 4, ex), lazy(n_ * 4, em) {
+        int x = 1;
+        while (n_ > x) x *= 2;
+        n = x;
+    }
+
+    void set(int i, X x) { dat[i + n - 1] = x; }
+    void build() {
+        for (int k = n - 2; k >= 0; k--) dat[k] = fx(dat[2 * k + 1], dat[2 * k + 2]);
+    }
+
+    /* lazy eval */
+    void eval(int k) {
+        if (lazy[k] == em) return;  // 更新するものが無ければ終了
+        if (k < n - 1) {            // 葉でなければ子に伝搬
+            lazy[k * 2 + 1] = fm(lazy[k * 2 + 1], lazy[k]);
+            lazy[k * 2 + 2] = fm(lazy[k * 2 + 2], lazy[k]);
+        }
+        // 自身を更新
+        dat[k] = fa(dat[k], lazy[k]);
+        lazy[k] = em;
+    }
+
+    void update(int a, int b, M x, int k, int l, int r) {
+        eval(k);
+        if (a <= l && r <= b) {  // 完全に内側の時
+            lazy[k] = fm(lazy[k], x);
+            eval(k);
+        } else if (a < r && l < b) {                     // 一部区間が被る時
+            update(a, b, x, k * 2 + 1, l, (l + r) / 2);  // 左の子
+            update(a, b, x, k * 2 + 2, (l + r) / 2, r);  // 右の子
+            dat[k] = fx(dat[k * 2 + 1], dat[k * 2 + 2]);
+        }
+    }
+    void update(int a, int b, M x) { update(a, b, x, 0, 0, n); }
+
+    X query_sub(int a, int b, int k, int l, int r) {
+        eval(k);
+        if (r <= a || b <= l) {  // 完全に外側の時
+            return ex;
+        } else if (a <= l && r <= b) {  // 完全に内側の時
+            return dat[k];
+        } else {  // 一部区間が被る時
+            X vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
+            X vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
+            return fx(vl, vr);
+        }
+    }
+    X query(int a, int b) { return query_sub(a, b, 0, 0, n); }
+};
+
+int GCD(int a, int b)
+{
+  return b ? GCD(b, a % b) : a;
+}
+
+// 区間更新型のRMQを構築
+using X = int;
+using M = int;
+auto fx = [](X x1, X x2) -> X { return GCD(x1, x2); };
+auto fa = [](X x, M m) -> X { return m; };
+auto fm = [](M m1, M m2) -> M { return m2; };
+// int ex = numeric_limits<int>::max();
+int ex = 0;
+int em = numeric_limits<int>::max();
+
+
+int main(){
+  int n; cin >> n;
+  
+  SegTreeLazy<X, M> seg(n, fx, fa, fm, ex, em);
+  vector<int> a(n);
+  for (auto &v : a)
+    cin >> v;
+  for (int i = 0; i < n; i++)
+  {
+    seg.set(i, a[i]);
+  }
+  seg.build();
+  int ans = 0;
+  for (int i = 0; i < n; i++)
+  {
+    int left = seg.query(0,i);
+    int right = seg.query(i+1,n);
+    int tem = GCD(left, right);
+    chmax(ans, tem);
+  }
+
+  cout << ans << endl;
+  return 0;
+}
