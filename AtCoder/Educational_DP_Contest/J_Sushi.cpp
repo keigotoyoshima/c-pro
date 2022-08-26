d8d4a38541bce5da449d5f43494db6e21f6b91b4
keigotoyo <keigo.work199987s@gmail.com>
2022-08-26 18:42:40 +0900

    rename

---
+++ AtCoder/Educational_DP_Contest/J_Sushi.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <assert.h>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+int inf_int = 2147483647;
+const int MOD = 1000000007;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
+
+
+// 解答読んでも理解できない後回し
+int main(){
+  int n; cin >> n; 
+  return 0;
+}
