c8ac0d084db9dc3b56ad5d66083efeb98e8e5779
keigotoyo <keigo.work199987s@gmail.com>
2022-08-26 18:32:51 +0900

    行列累乗と繰り返し二乗法

--- AtCoder/Educational_DP_Contest/R_Walk.cpp
+++ AtCoder/Educational_DP_Contest/R_Walk.cpp
@@ -28,9 +28,73 @@ const int dy[] = {0, 1, 0, -1};
 const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
 const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
 
-// 行列累乗
 
-int main(){
+int n;
+// 繰り返し二乗法
+vector<vector<ll>> modPow(vector<vector<ll>>a, ll k)
+{
+  vector<vector<ll>> res(n, vector<ll>(n, 0));
+  vector<vector<ll>> tem;
+
+  if (k == 1){
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+        a[i][j] %= MOD;
+      }
+    }
+    return a;
+  }
+   
+  if (k % 2 == 1){
+    tem = modPow(a, k - 1);
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        for (int g = 0; g < n; g++)
+        {
+          res[i][j] += (a[i][g] * tem[g][j]) % MOD;
+          res[i][j] %= MOD;
+        }
+      }
+    }
+    return res;
+  }
+   
+
+  tem = modPow(a, k / 2);
+ 
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      for(int g = 0; g < n; g++){
+        res[i][j] += (tem[i][g] * tem[g][j]) % MOD;
+        res[i][j] %= MOD;
+      }
+    }
+  }
+
+  return res;
+}
+
+// 行列累乗
+int main(){ 
+  ll k;
+  cin >> n >> k;
+  vector<vector<ll>>A(n, vector<ll>(n,0));
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      cin >> A[i][j];
+    }
+  }
+  vector<vector<ll>> res = modPow(A, k);
+  ll ans = 0;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      ans += (res[i][j] % MOD);
+      ans %= MOD;
+    }
+  }
+  cout << ans << endl;
 
   return 0;
 }
