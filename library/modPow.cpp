cdbf4ecf76074b3587d0d58259ba9071ede7fa3b
keigotoyo <keigo.work199987s@gmail.com>
2022-08-26 12:46:13 +0900

    微修正

--- library/modPow.cpp
+++ library/modPow.cpp
@@ -1,12 +1,12 @@
-// 繰り返し二乗法
-int modPow(long long a, long long n, long long p)
+// 繰り返し二乗法(aのn乗)
+int modPow(long long a, long long n)
 {
   if (n == 0)
     return 1; // 0乗にも対応する場合
   if (n == 1)
-    return a % p;
+    return a % MOD;
   if (n % 2 == 1)
-    return (a * modPow(a, n - 1, p)) % p;
-  long long t = modPow(a, n / 2, p);
-  return (t * t) % p;
+    return (a * modPow(a, n - 1)) % MOD;
+  long long t = modPow(a, n / 2);
+  return (t * t) % MOD;
 }
