21c573a87beec65c10f5ac22ac7b304caf46afd1
keigotoyo <keigo.work199987s@gmail.com>
2022-10-23 22:47:24 +0900

    overflow　途中計算

---
+++ AtCoder/question/overflow.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <assert.h>
+#include <functional>
+using namespace std;
+using ll = long long;
+#define fi first
+#define se second
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+int inf_int = 2147483647;
+const int MOD = 1000000007;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+const int dx8[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dy8[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+int main()
+{
+  int a = 2000000000;
+  cout << a * 2 << endl;
+  int b = a * 2;
+  cout << b << endl;
+  ll c = (ll)a * 2;
+  cout << c << endl;
+}
